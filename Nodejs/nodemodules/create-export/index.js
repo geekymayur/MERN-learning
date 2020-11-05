93de2fef0add03bf4189a9641b77a0aafbec36c8
geekymayur <rajmayur219@gmail.com>
2020-11-05 22:00:13 +0530

    use node modules and create your own modules

---
+++ Nodejs/nodemodules/create-export/index.js
@@ -0,0 +1,6 @@
+let { sum, multiplication } = require("./mymodules/math");
+
+
+console.log(sum(5, 2));
+
+console.log(multiplication(6, 8));
---
+++ Nodejs/nodemodules/create-export/mymodules/math.js
@@ -0,0 +1,7 @@
+let sum = function (a, b) { return a + b };
+let multiplication = function (a, b) { return a * b };
+
+
+
+
+module.exports = {sum,multiplication};
